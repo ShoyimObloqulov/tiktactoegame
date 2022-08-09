b02f9a5e44d7ee10c21144ea00670dc9270b59d3
Shoyim Obloqulov <92427513+ShoyimObloqulov@users.noreply.github.com>
2022-08-09 21:00:47 +0500

    Add files via upload

---
+++ Tic-Tac-Toe/index.php
@@ -0,0 +1,23 @@
+<?php
+	function ArraySum($arr)
+	{
+		$s = array_sum($arr);
+		return $s 	
+	} 
+	function ArraySumOne($arr)
+	{
+		$s = 0;
+		foreach ($variable as $arr) {
+				$s += $variable;
+		}	
+		return $s;
+	} 
+	function ArraySumTwo($arr)
+	{
+		$s = 0;
+		for ($i=0; $i < count($arr); $i++) { 
+			$s += $arr[$i];
+		}
+		return $s;
+	}
+?>
